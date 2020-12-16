1827c36dbeaec5483ee16c19550f7d026cafd604
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-12-16 22:37:20 +0900

    벨로서티 적용
    벨로서티 적용

--- app/src/main/java/com/example/recyclerviewswipemenu/SwipeHelperCallback.kt
+++ app/src/main/java/com/example/recyclerviewswipemenu/SwipeHelperCallback.kt
@@ -1,8 +1,11 @@
 package com.example.recyclerviewswipemenu
 
+import android.graphics.Canvas
+import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_swipe.view.*
 
 class SwipeHelperCallback : ItemTouchHelper.Callback() {
 
@@ -21,8 +24,51 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
         target: RecyclerView.ViewHolder
     ): Boolean = false
 
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) { }
 
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        getDefaultUIUtil().clearView(getView(viewHolder))
     }
 
+    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        viewHolder?.let {
+            getDefaultUIUtil().onSelected(getView(it))
+        }
+    }
+
+    override fun onChildDraw(
+        canvas: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            val view = getView(viewHolder)
+
+            getDefaultUIUtil().onDraw(
+                canvas,
+                recyclerView,
+                view,
+                dX,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+        }
+    }
+
+    private fun getView(viewHolder: RecyclerView.ViewHolder): View {
+        return (viewHolder as SwipeListAdapter.SwipeViewHolder).itemView.ll_swipe_view
+    }
+
+    override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
+        return defaultValue * 10
+    }
+
+    override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
+        return 2f
+    }
 }
